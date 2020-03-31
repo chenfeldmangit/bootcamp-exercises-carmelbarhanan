dc2a1917fb95f349caafd1acc85962c7e69e32b8
Carmel Bar-hanan <carmel.barhanan@gong.io>
2020-03-31 16:15:22 +0300

    -

--- twitter/scripts/twitterScripts.js
+++ twitter/scripts/twitterScripts.js
@@ -31,7 +31,6 @@ window.onload = function () {
 
     document.getElementsByClassName('profile-name')[0].innerHTML = profileInfo.profileName;
     document.getElementsByClassName('profile-name')[1].innerHTML = profileInfo.profileName;
-
     document.getElementById('numOfTweets').innerText = profileInfo.numOfTweets + ' Tweets';
     document.getElementById('profilePage').src = profileInfo.profilePhotoPath;
     document.getElementById('coverPhoto').src = profileInfo.coverPhotoPath;
@@ -42,10 +41,119 @@ window.onload = function () {
     document.getElementsByClassName('personal-description')[2].innerHTML = profileInfo.joinedTime;
     document.getElementsByClassName('follow-info')[0].innerHTML = profileInfo.numOfFollowing + ' Following';
     document.getElementsByClassName('follow-info')[1].innerHTML = profileInfo.numOfFollowers + ' Followers';
-
-
-
     var div = document.createElement("div");
     document.getElementById('middle-stream-profile').appendChild(div)
 
 };
+
+tweetsList = [{
+    profilePhotoPathToTweeter: "profile.jpg",
+    tweeterName: "Carmel Bar-Hanan",
+    tweetContent: "The seaweed is always greener, In somebody else's lake. You dream about going up there, But that is a big mistake",
+    timeOfTweet: "March 30 2020, 14:20",
+    numberOfLikes: 1
+}, {
+    profilePhotoPathToTweeter: "profile2.jpg",
+    tweeterName: "Etai Bar-Hanan",
+    tweetContent: "Down here all the fish is happy, As off through the waves they roll. The fish on the land ain't happy, They sad 'cause they in their bowl",
+    timeOfTweet: "March 31 2020, 10:41",
+    numberOfLikes: 4
+}];
+
+window.onload = function setTweets() {
+    localStorage.setItem('tweetsList', JSON.stringify(tweetsList));
+    Tweets.loadTweets();
+    // setInterval(()=>{
+    //     Tweets.loadTweets()
+    // }, 1000)
+};
+
+
+
+class Tweets {
+     static saveNewTweet() {
+        var tweetInput = document.getElementById("tweetInput").value;
+         this.getTweets().then(tweetsList =>
+         tweetsList[Object.keys(tweetsList).length] = {
+             profilePhotoPathToTweeter: "profile.jpg",
+             tweeterName: "Carmel Bar-Hanan",
+             tweetContent: tweetInput,
+             timeOfTweet: this.getNowTime(),
+             numberOfLikes: 0
+         });
+         localStorage.setItem('tweetsList', JSON.stringify(tweetsList));
+         document.getElementById("tweetInput").place = "";
+         this.loadNewTweet();
+     }
+
+    static getNowTime() {
+        var today = new Date();
+        var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+        var time = today.getHours() + ":" + today.getMinutes();
+        return date+' '+time;
+    }
+
+    static loadTweets() {
+        document.getElementsByTagName("template").innerHTML = "";
+        this.getTweets().then(tweetsList => {
+        let temp = document.getElementsByTagName("template")[0];
+        for (var i=0; i < Object.keys(tweetsList).length; i++) {
+            let clon = temp.content.cloneNode(true);
+            clon.querySelector("article").setAttribute("id", "tweet" + i);
+            clon.querySelector("#profilePicOfTweeter").src = tweetsList[i].profilePhotoPathToTweeter;
+            clon.querySelector("#TweetAuthor").innerHTML = tweetsList[i].tweeterName;
+            clon.querySelector("#tweenContent").innerHTML = tweetsList[i].tweetContent;
+            clon.querySelector("#timeOfTweet").innerHTML = tweetsList[i].timeOfTweet;
+            clon.querySelector("#numOfLikes").innerHTML = tweetsList[i].numberOfLikes;
+            document.getElementById("tweets").appendChild(clon);
+         }
+        });
+     }
+
+    static getTweets = () => {
+         return new Promise((resolve, reject) => {
+             try {
+                 let tweetData = JSON.parse(localStorage.getItem('tweetsList') ) || {};
+                 resolve(tweetData);
+             } catch (e) {
+                 reject(e);
+             }
+         })
+    };
+
+    static loadNewTweet() {
+        this.getTweets().then(tweetsList => {
+            let temp = document.getElementsByTagName("template")[0];
+            let clon = temp.content.cloneNode(true);
+            var tweetsListSize = Object.keys(tweetsList).length;
+            clon.querySelector("article").setAttribute("id", "tweet" + tweetsListSize);
+            clon.querySelector("#profilePicOfTweeter").src = tweetsList[tweetsListSize - 1].profilePhotoPathToTweeter;
+            clon.querySelector("#TweetAuthor").innerHTML = tweetsList[tweetsListSize - 1].tweeterName;
+            clon.querySelector("#tweenContent").innerHTML = tweetsList[tweetsListSize - 1].tweetContent;
+            clon.querySelector("#timeOfTweet").innerHTML = tweetsList[tweetsListSize - 1].timeOfTweet;
+            clon.querySelector("#numOfLikes").innerHTML = tweetsList[tweetsListSize - 1].numberOfLikes;
+            document.getElementById("tweets").appendChild(clon);
+        });
+    }
+
+    static addLikeToTweet(obj) {
+        var idOfTweet = obj.parentElement.parentElement.getAttribute("id");
+        var numOfTweet = idOfTweet.split('tweet')[1];
+        this.getTweets().then(tweetsList => {
+            tweetsList[numOfTweet].numberOfLikes += 1;
+            localStorage.setItem('tweetsList', JSON.stringify(tweetsList));
+        });
+    }
+
+    static deleteTweet(obj) {
+        var idOfTweet = obj.parentElement.parentElement.getAttribute("id");
+        var numOfTweet = idOfTweet.split('tweet')[1];
+        this.getTweets().then(tweetsList => {
+            var a = tweetsList.splice[numOfTweet];
+            localStorage.setItem('tweetsList', JSON.stringify(tweetsList));
+        });
+    }
+}
+
+
+
